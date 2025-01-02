f292628e598c7db6e05e41e8f7aa50e96633c105
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2025-01-02 16:52:38 +0800

    init: Vite + React + Electron + Tailwind + ShadCN

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/electron-vite.animate.svg"
+
+function App() {
+	const [count, setCount] = useState(0)
+
+	return (
+		<div>
+			
+		</div>
+	)
+}
+
+export default App
